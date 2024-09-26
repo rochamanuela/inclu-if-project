97a4f174d06fdbfc8a2b37e377bcd4a9c09aa487
rochamanuela <rochadesouzamanuela@gmail.com>
2024-09-26 13:50:54 -0300

    Create navbar for mobile

---
+++ script.js
@@ -0,0 +1,16 @@
+// Variables to select elements
+const menuIcon = document.getElementById('menu-icon')
+const closeMenuIcon = document.getElementById('close-menu-icon')
+const internalLinks = document.getElementById('internal-links')
+const overlay = document.getElementById('overlay')
+
+// Function to toggle the menu
+function toggleMenu() {
+    internalLinks.classList.toggle('open')
+    overlay.classList.toggle('open')
+}
+
+// Event listeners for opening/closing the menu
+menuIcon.addEventListener('click', toggleMenu)
+closeMenuIcon.addEventListener('click', toggleMenu)
+overlay.addEventListener('click', toggleMenu)
