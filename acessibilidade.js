5882214f4faf71940fd6a75a3ac09ea87f41318d
Pablo Duque <pabloduquesantos@gmail.com>
2024-10-05 18:47:03 -0300

    Mudar tamanho de linha

--- acessibilidade.js
+++ acessibilidade.js
@@ -13,6 +13,11 @@ const decreaseSizeButton = document.querySelector('.decrease-size');
 increaseSizeButton.addEventListener('click', () => changeSize('increase'))
 decreaseSizeButton.addEventListener('click', () => changeSize('decrease'))
 
+const increaseRowSizeButton = document.querySelector('.increase-row');
+const decreaseRowSizeButton = document.querySelector('.decrease-row');
+increaseRowSizeButton.addEventListener('click', () => changeRowSize('increase'))
+decreaseRowSizeButton.addEventListener('click', () => changeRowSize('decrease'))
+
 function removeAllImages() {
     images.forEach(image => {
         image.classList.toggle('no_image');
@@ -30,13 +35,26 @@ function toggleColor() {
 
 function changeSize(operation){
     if(operation == 'increase')
+        value = 1.3;
+    else
+        value = 0.7;
+
+    text.forEach(element => {
+        const style = window.getComputedStyle(element);
+        const oldSize = parseFloat(style.fontSize);
+        element.style.fontSize = `${oldSize * value}px`;
+    });
+}
+
+function changeRowSize(operation){
+    if(operation == 'increase')
         value = 1.1;
     else
         value = 0.9;
 
     text.forEach(element => {
         const style = window.getComputedStyle(element);
-        const oldSize = parseFloat(style.fontSize);
-        element.style.fontSize = `${oldSize * value}px`;
+        const oldSize = parseFloat(style.lineHeight);
+        element.style.lineHeight = `${oldSize * value}px`;
     });
 }
